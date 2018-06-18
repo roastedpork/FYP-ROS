820f9b994035974c46d96069e076c802759c4c67
Chan Bingjie <jienet888@gmail.com>
2018-06-18 16:54:28 +0100

    prep for finale

--- scripts/wheelchair_pose_publisher.py
+++ scripts/wheelchair_pose_publisher.py
@@ -5,39 +5,46 @@ import numpy as np
 import math 
 import tf
 
-from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Pose
 
 class Params:
     rate = 10.0
-    floor = -0.7
       
 # Main loop
 def main():
     # Initialize the Formatted Grid node
-    rospy.init_node('minimap_processor', anonymous=True)
+    rospy.init_node('wheelchair_publisher_node', anonymous=True)
     
+
+    pub = rospy.Publisher("/hololens/wheelchair_pose", Pose, queue_size=10)
     rate = rospy.Rate(Params.rate)
 
     
     listener = tf.TransformListener()
     
     while not rospy.is_shutdown():
-        try:
-            curr_time = rospy.Time(0)
-            (position, rotation) = listener.lookupTransform('/odom', '/base_link', curr_time)
-            
-            theta = tf.transformations.euler_from_quaternion(rotation)[2]
-            
-            msg = Pose2D()
-            msg.x = round(position[0], 3)
-            msg.y = round(position[1], 3)
-            msg.theta = round(theta, 3)
-            
-            
-            
-            
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            continue
+        if listener.frameExists("/Unity"):
+            try:
+                curr_time = rospy.Time(0)
+                (position, rotation) = listener.lookupTransform('/base_link', '/Unity', curr_time)
+                
+                
+                msg = Pose()
+                msg.position.x = round(position[0], 3)
+                msg.position.y = round(position[1], 3)
+                msg.position.z = round(position[2], 3)
+
+                msg.orientation.x = round(rotation[0], 3)                
+                msg.orientation.y = round(rotation[1], 3)
+                msg.orientation.z = round(rotation[2], 3)
+                msg.orientation.w = round(rotation[3], 3)
+
+                pub.publish(msg)
+                
+                
+                
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
         
         rate.sleep()
         
